4e1e8c37cacd41947478827769a5e4b6a73293b2
码帅 <2690438874@qq.com>
2023-09-30 08:22:09 +0800

    modify README.md

--- README.md
+++ README.md
@@ -5,18 +5,20 @@
 It should be noted that different models do not have universal training code, so if you want to run the program, you need to make corresponding code adjustments based on your actual situation (assuming you have basic knowledge of deep learning and the PyTorch framework).
 If you want to train with your own experimental data, you need to write the corresponding dataloader.
 More details depend on your actual running environment.*
+
+### Model architecture
+![modelframework.png](img%2Fmodelframework.png)
 ### Data:AQI time series of Yan 'an from 2014 to 2019
-在data文件夹下放置了实验需要的延安市空气污染数据集
+The air pollution data set of Yan 'an city required by the experiment is placed under the data folder
 
 ### Models folder:
+- Informer folder
 - RNN
 - LSTM
 - TCN
-- Transformer Encoder
-- TE-Informer
-
-![modelframework.png](img%2Fmodelframework.png)
-
+- MLP
+- former
+- other modules
 
 ### Utils folder
-在该文件夹下存放了评估函数，绘图函数等相关工具
+In this folder are stored time coding function, evaluation function, drawing function and other related tools
--- utils/time_encode.py
+++ utils/time_encode.py
@@ -1,5 +1,6 @@
 import datetime
-
+import torch
+import torch.nn as nn
 import numpy as np
 import pandas as pd
 # Trigonometric coding of time
@@ -16,3 +17,29 @@ def process_data(self, source=r'Air_AQI_yanan.csv'):
     df['sin_month'] = np.sin(2 * np.pi * timestamps_month / month_in_year)
     df['cos_month'] = np.cos(2 * np.pi * timestamps_month / month_in_year)
     return df
+
+class TrigonometricEncoding(nn.Module):
+    def __init__(self, source=r'Air_AQI_yanan.csv'):
+        super(TrigonometricEncoding, self).__init__()
+        self.source = source
+
+    def forward(self, x):
+        df = pd.read_csv(self.source)
+        timestamps = [ts.split('+')[0] for ts in df['Date']]
+        timestamps_day = np.array([float(datetime.datetime.strptime(t, '%Y-%m-%d').day) for t in timestamps])
+        timestamps_month = np.array([float(datetime.datetime.strptime(t, '%Y-%m-%d').month) for t in timestamps])
+        days_in_month = 30
+        month_in_year = 12
+        df['sin_day'] = np.sin(2 * np.pi * timestamps_day / days_in_month)
+        df['cos_day'] = np.cos(2 * np.pi * timestamps_day / days_in_month)
+        df['sin_month'] = np.sin(2 * np.pi * timestamps_month / month_in_year)
+        df['cos_month'] = np.cos(2 * np.pi * timestamps_month / month_in_year)
+        # Assuming x is a placeholder for your input data (you need to replace this with your actual input)
+        # You can return the processed data as a tensor
+        return torch.tensor(df.values, dtype=torch.float32)
+
+# Usage
+source_file = 'Air_AQI_yanan.csv'
+data_processor = TrigonometricEncoding(source=source_file)
+input_data = torch.randn(10, 10) 
+output_data = data_processor(input_data)
